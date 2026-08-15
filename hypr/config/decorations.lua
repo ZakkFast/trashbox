f53a550604a8d83067fe5bd2363aebc291eba6b2
Zakk Fast <Zakkfastpro@gmail.com>
2026-08-14 23:51:33 -0500

    Add dynamic Noctalia theme integration

--- hypr/config/decorations.lua
+++ hypr/config/decorations.lua
@@ -3,7 +3,7 @@
 hl.config({
     general = {
         gaps_in = 3,
-        gaps_out = 8,
+        gaps_out = 3,
         border_size = 2,
         extend_border_grab_area = 10,
         resize_on_border = true,
@@ -33,13 +33,13 @@ hl.config({
     },
     decoration = {
         dim_special = 0.3,
-        rounding = 10,
-        active_opacity = 0.95,
+        rounding = 6,
+        active_opacity = 1.0,
         inactive_opacity = 0.85,
         fullscreen_opacity = 1,
         blur = {
-            size = 5,
-            passes = 4,
+            size = 6,
+            passes = 3,
             special = true,
         },
     },
