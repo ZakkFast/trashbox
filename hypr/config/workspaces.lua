136d63fbda24918244693a5a3541e36038ae0154
Zakk Fast <Zakkfastpro@gmail.com>
2026-08-14 23:02:43 -0500

    Customize TrashBox workspaces

--- hypr/config/workspaces.lua
+++ hypr/config/workspaces.lua
@@ -1,9 +1,38 @@
 -- Workspace rules wiki https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 -- Add your workspace rules here. Increment the workspace number as you go. Do not have duplicate workspaces.
-hl.workspace_rule({ workspace = "name:gaming", monitor = PRIMARY_MONITOR, default = true })
-hl.workspace_rule({ workspace = "1", monitor = MONITOR1, default = true, persistent = true })
-hl.workspace_rule({ workspace = "2", monitor = MONITOR1, default = true, persistent = true })
-hl.workspace_rule({ workspace = "3", monitor = MONITOR1, default = true, persistent = true })
--- hl.workspace_rule({ workspace = "4", monitor = MONITOR2, default = true, persistent = true })
--- hl.workspace_rule({ workspace = "5", monitor = MONITOR2, default = true, persistent = true })
--- hl.workspace_rule({ workspace = "6", monitor = MONITOR2, default = true, persistent = true })
+
+hl.workspace_rule({
+    workspace = "1",
+    monitor = MONITOR1,
+    default = true,
+    persistent = true,
+    default_name = "DEV",
+})
+
+hl.workspace_rule({
+    workspace = "2",
+    monitor = MONITOR1,
+    persistent = true,
+    default_name = "SERVER",
+})
+
+hl.workspace_rule({
+    workspace = "3",
+    monitor = MONITOR1,
+    persistent = true,
+    default_name = "NOT_PORN",
+})
+
+hl.workspace_rule({
+    workspace = "4",
+    monitor = MONITOR1,
+    persistent = true,
+    default_name = "GAMING",
+})
+
+hl.workspace_rule({
+    workspace = "5",
+    monitor = MONITOR1,
+    persistent = true,
+    default_name = "BRAIN_ROT",
+})
